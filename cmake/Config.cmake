6a1094003eba61d6bbece5d6296a8671b1c51288
Alejandro Ricoveri <alejandroricoveri@gmail.com>
2013-05-03 08:33:52 -0430

    CMake initial setup

---
+++ cmake/Config.cmake
@@ -0,0 +1,53 @@
+#
+# division : A minimalist FPS game
+# Copyright (c) 2012, Alejandro Ricoveri
+#
+# Routine for OS and compiler detection
+# Thanks to SFML guys (www.sfml-dev.org)
+#
+
+# detect the OS
+if(${CMAKE_SYSTEM_NAME} MATCHES "Windows")
+    set(WINDOWS 1)
+
+    # detect the architecture (note: this test won't work for cross-compilation)
+    include(CheckTypeSize)
+    check_type_size(void* SIZEOF_VOID_PTR)
+    if("${SIZEOF_VOID_PTR}" STREQUAL "4")
+        set(ARCH_32BITS 1)
+    elseif("${SIZEOF_VOID_PTR}" STREQUAL "8")
+        set(ARCH_64BITS 1)
+    else()
+        message(FATAL_ERROR "Unsupported architecture")
+        return()
+    endif()
+elseif(${CMAKE_SYSTEM_NAME} MATCHES "Linux")
+    set(LINUX 1)
+elseif(${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD")
+    # FreeBSD compile path is the same as Linux
+    set(LINUX 1)
+elseif(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
+    set(MACOSX 1)
+
+    # detect OS X version. (use '/usr/bin/sw_vers -productVersion' to extract V from '10.V.x'.)
+    EXEC_PROGRAM(/usr/bin/sw_vers ARGS -productVersion OUTPUT_VARIABLE MACOSX_VERSION_RAW)
+    STRING(REGEX REPLACE "10\\.([0-9]).*" "\\1" MACOSX_VERSION "${MACOSX_VERSION_RAW}")
+    if(${MACOSX_VERSION} LESS 7)
+        message(FATAL_ERROR "Unsupported version of OS X : ${MACOSX_VERSION_RAW}")
+        return()
+    endif()
+else()
+    message(FATAL_ERROR "Unsupported operating system")
+    return()
+endif()
+
+# detect the compiler and its version
+# Note: gcc is the only compiler supported by this time
+if(CMAKE_COMPILER_IS_GNUCXX)
+    set(COMPILER_GCC 1)
+    execute_process(COMMAND "${CMAKE_CXX_COMPILER}" "-dumpversion" OUTPUT_VARIABLE GCC_VERSION_OUTPUT)
+    string(REGEX REPLACE "([0-9]+\\.[0-9]+).*" "\\1" GCC_VERSION "${GCC_VERSION_OUTPUT}")
+else()
+    message(FATAL_ERROR "Unsupported compiler")
+    return()
+endif()
